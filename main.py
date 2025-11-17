5396f8fb4d385fad603bc71da15826df1fafbc3c
jim-eaton-home <jimeatononline@gmail.com>
2025-11-17 18:47:38 -0500

    added sound

--- main.py
+++ main.py
@@ -9,16 +9,14 @@ def on_button_pressed_a():
         basic.clear_screen()
         basic.show_icon(IconNames.SMALL_SQUARE)
     else:
+        music.play(music.string_playable("C - C - C - C5 - ", 120),
+            music.PlaybackMode.UNTIL_DONE)
         # press count is odd...let's get this party started
         letsGo = True
         started = False
-        basic.clear_screen()
-        basic.show_string("!")
-        distanceToBad = robot.obstacle_distance()
         while letsGo and not (pressedButtonB):
-            #short pause to allow the processor to catch up the events
+            # short pause to allow the processor to catch up the events
             basic.pause(10)
-
             distanceToBad = robot.obstacle_distance()
             if distanceToBad <= 30:
                 robot_avoid()
@@ -26,10 +24,13 @@ def on_button_pressed_a():
             if not (started):
                 robot.motor_steer(0, 15)
                 started = True
+                basic.show_string("GORT!!!")
+                basic.show_string("!")
+                basic.clear_screen()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
-    global letsGo, distanceToBad, pressedButtonB, pressCountA
+    global letsGo, distanceToBad, pressCountA, pressedButtonB
     robot.motor_stop()
     letsGo = False
     distanceToBad = 0
@@ -44,13 +45,30 @@ def on_button_pressed_b():
     robot.motor_stop()
     pressedButtonB = not (pressedButtonB)
     basic.pause(100)
+    music.play(music.string_playable("C5 B A G F E D C ", 120),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.create_sound_expression(WaveShape.SINE,
+            1,
+            5000,
+            100,
+            255,
+            500,
+            SoundExpressionEffect.NONE,
+            InterpolationCurve.LINEAR),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.BREVE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music._play_default_background(music.built_in_playable_melody(Melodies.DADADADUM),
+        music.PlaybackMode.IN_BACKGROUND)
+    music.play(music.builtin_playable_sound_effect(soundExpression.giggle),
+        music.PlaybackMode.UNTIL_DONE)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def robot_avoid():
     # the simulator says this will be right turn
     robot.motor_stop()
     robot.motor_tank(100, -100, 175)
-    robot.motor_stop() 
+    robot.motor_stop()
 
 pressedButtonB = False
 letsGo = False
--- main.ts
+++ main.ts
@@ -1,22 +1,19 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+input.onButtonPressed(Button.A, function () {
     let started: boolean;
-    
-    pressCountA += 1
+pressCountA += 1
     if (pressCountA % 2 == 0) {
-        //  press count even, that means we want to turn it off...all ahead Car_Stop
+        // press count even, that means we want to turn it off...all ahead Car_Stop
         distanceToBad = 0
         letsGo = false
         robot.motorStop()
         basic.clearScreen()
         basic.showIcon(IconNames.SmallSquare)
     } else {
-        //  press count is odd...let's get this party started
+        music.play(music.stringPlayable("C - C - C - C5 - ", 120), music.PlaybackMode.UntilDone)
+        // press count is odd...let's get this party started
         letsGo = true
         started = false
-        basic.clearScreen()
-        basic.showString("!")
-        distanceToBad = robot.obstacleDistance()
-        while (letsGo && !pressedButtonB) {
+        while (letsGo && !(pressedButtonB)) {
             // short pause to allow the processor to catch up the events
             basic.pause(10)
             distanceToBad = robot.obstacleDistance()
@@ -24,18 +21,17 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
                 robot_avoid()
                 started = false
             }
-            
-            if (!started) {
+            if (!(started)) {
                 robot.motorSteer(0, 15)
                 started = true
+                basic.showString("GORT!!!")
+                basic.showString("!")
+                basic.clearScreen()
             }
-            
         }
     }
-    
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
+input.onButtonPressed(Button.B, function () {
     robot.motorStop()
     letsGo = false
     distanceToBad = 0
@@ -48,18 +44,21 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
     } else {
         basic.showIcon(IconNames.Asleep)
     }
-    
     robot.motorStop()
-    pressedButtonB = !pressedButtonB
+    pressedButtonB = !(pressedButtonB)
     basic.pause(100)
+    music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.createSoundExpression(WaveShape.Sine, 1, 5000, 100, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
+    music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
 })
-function robot_avoid() {
-    //  the simulator says this will be right turn
+function robot_avoid () {
+    // the simulator says this will be right turn
     robot.motorStop()
     robot.motorTank(100, -100, 175)
     robot.motorStop()
 }
-
 let pressedButtonB = false
 let letsGo = false
 let pressCountA = 0
