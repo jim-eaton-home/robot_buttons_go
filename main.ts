92718af2bbb42990419c2d792a87ba4c1d64fa36
jim-eaton-home <jimeatononline@gmail.com>
2025-11-17 19:41:54 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,69 +1,40 @@
 input.onButtonPressed(Button.A, function () {
-    let started: boolean;
-pressCountA += 1
-    if (pressCountA % 2 == 0) {
-        // press count even, that means we want to turn it off...all ahead Car_Stop
-        distanceToBad = 0
-        letsGo = false
+    music.play(music.stringPlayable("C D E C5 C5 B A G ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("C D E C5 B A G C ", 120), music.PlaybackMode.UntilDone)
+    for (let index = 0; index < 4; index++) {
+        robot.motorTank(50, 50, 2000)
         robot.motorStop()
-        basic.clearScreen()
-        basic.showIcon(IconNames.SmallSquare)
-    } else {
-        music.play(music.stringPlayable("C - C - C - C5 - ", 120), music.PlaybackMode.UntilDone)
-        // press count is odd...let's get this party started
-        letsGo = true
-        started = false
-        while (letsGo && !(pressedButtonB)) {
-            // short pause to allow the processor to catch up the events
-            basic.pause(10)
-            distanceToBad = robot.obstacleDistance()
-            if (distanceToBad <= 30) {
-                robot_avoid()
-                started = false
-            }
-            if (!(started)) {
-                robot.motorSteer(0, 15)
-                started = true
-                basic.showString("GORT!!!")
-                basic.showString("!")
-                basic.clearScreen()
-            }
-        }
+        robot.motorTank(0, -75, 500)
+        robot.motorStop()
     }
 })
 input.onButtonPressed(Button.B, function () {
-    robot.motorStop()
-    letsGo = false
-    distanceToBad = 0
-    robot.motorStop()
-    basic.clearScreen()
-    if (pressedButtonB) {
-        pressCountA += 1
-        robot_avoid()
-        basic.showIcon(IconNames.Skull)
-    } else {
-        basic.showIcon(IconNames.Asleep)
+    circleCounter = 0
+    for (let index = 0; index < 14; index++) {
+        circleCounter += 1
+        robot.motorSteer(28, 25)
+        basic.showNumber(circleCounter)
     }
     robot.motorStop()
-    pressedButtonB = !(pressedButtonB)
-    basic.pause(100)
-    music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
-    music.play(music.createSoundExpression(WaveShape.Sine, 1, 5000, 100, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
-    music.play(music.tonePlayable(262, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
-    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
-    music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
+    robot.motorTank(0, -75, 500)
+    robot.motorStop()
+    robot.motorTank(27, 33, 5000)
+    robot.motorStop()
 })
-function robot_avoid () {
-    // the simulator says this will be right turn
-    robot.motorStop()
-    robot.motorTank(100, -100, 175)
-    robot.motorStop()
-}
-let pressedButtonB = false
-let letsGo = false
-let pressCountA = 0
-let distanceToBad = 0
-robot.yahboomTinyBit.start()
-distanceToBad = 10
+let circleCounter = 0
 basic.clearScreen()
-basic.showIcon(IconNames.Surprised)
+basic.showLeds(`
+    # # # # #
+    # # . # #
+    . . . . .
+    # . . . #
+    # # # # #
+    `)
+basic.showLeds(`
+    # # # # #
+    # # . # #
+    . . . . .
+    # # # # #
+    . . . # #
+    `)
+robot.yahboomTinyBit.start()
